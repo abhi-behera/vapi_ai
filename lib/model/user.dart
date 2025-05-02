1d2ccc822dfe2f6f420c95c59f5981dc0b40a426
abhi-behera <abhijitbehera1997@gmail.com>
2025-05-03 00:28:14 +0530

    UI changes and textbox added in place of list view

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -2,9 +2,12 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class VapiCallService {
-  final String apiKey = '35e637e2-9d39-4cb1-aca3-9b520f6d9c5b';
-  final String assistantId = '80fee333-2dd7-4b64-91b4-0ec950b35dbb';
-  final String phoneNumberId = '51fb11d9-0a2d-4df6-b84d-78823658ca2b';
+  final String apiKey =
+      '598d7ae6-b0fb-4102-858a-5675aae802b5'; //'35e637e2-9d39-4cb1-aca3-9b520f6d9c5b';
+  final String assistantId =
+      '9f89257c-82fa-4a7d-be4c-4c6014e0c126'; //mine  '80fee333-2dd7-4b64-91b4-0ec950b35dbb';
+  final String phoneNumberId =
+      '222663ec-c7ad-4d5c-9005-ec082c348284'; //mine '51fb11d9-0a2d-4df6-b84d-78823658ca2b';
 
   Future<void> makeOutboundCall(String customerNumber) async {
     final url = Uri.parse('https://api.vapi.ai/call');
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import '../model/user.dart';
 
 class UserListPage extends StatefulWidget {
-  const UserListPage({super.key});
+  UserListPage({super.key});
+  final GlobalKey<FormState> _formKey1 = GlobalKey();
 
   @override
   State<UserListPage> createState() => _UserListPageState();
@@ -11,26 +12,149 @@ class UserListPage extends StatefulWidget {
 
 class _UserListPageState extends State<UserListPage> {
   final VapiCallService callService = VapiCallService();
-  final List<User> users = [
-    User(name: 'Het', phone: '+918200170255'),
-    User(name: 'Abhijit', phone: '+917205856225'),
-  ];
+  // final List<User> users = [
+  //   User(name: 'Het', phone: '+918200170255'),
+  //   User(name: 'Abhijit', phone: '+917205856225'),
+  // ];
+
+  final TextEditingController _addrrssController =
+      TextEditingController(text: "+91");
+  final TextEditingController _addrrss1Controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _addrrssController.dispose();
+    _addrrss1Controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('Users'))),
-      body: Center(
-        child: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index) {
-            final user = users[index];
-            return ListTile(
-              leading: const Icon(Icons.person),
-              title: Text(user.name),
-              subtitle: Text(user.phone),
-              onTap: () => callService.makeOutboundCall(user.phone),
-            );
-          },
+      appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 11, 10, 20),
+          title: const Center(
+              child: Text(
+            'Vapi ai',
+            style: TextStyle(color: Colors.white70),
+          ))),
+      body: Container(
+        color: const Color.fromARGB(255, 11, 10, 20),
+        child: Form(
+          key: widget._formKey1,
+          child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                const Spacer(),
+                SizedBox(
+                  width: screenWidth * 0.9,
+                  child: const Text(
+                    "Please enter the number. We will  contact you shortly.",
+                    style: TextStyle(fontSize: 20, color: Colors.white70),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      width: screenWidth * 0.15,
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: TextFormField(
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontSize: 16,
+                        ),
+                        controller: _addrrssController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: screenWidth * 0.75,
+                      margin: const EdgeInsets.only(right: 10),
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: TextFormField(
+                        style: const TextStyle(
+                          color: Colors.white70,
+                          fontSize: 16,
+                        ),
+                        controller: _addrrss1Controller,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          focusColor: Colors.white70,
+                          suffixStyle: const TextStyle(color: Colors.white70),
+                          hintText: 'Enter the user\'s number',
+                          hintStyle: const TextStyle(color: Colors.white70),
+                          suffixIcon: IconButton(
+                            onPressed: _addrrss1Controller.clear,
+                            icon: const Icon(
+                              Icons.clear,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          labelText: 'Contact Box',
+                          labelStyle: const TextStyle(color: Colors.white70),
+                          border: const OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please  enter any number';
+                          } else if (value.length != 10) {
+                            return 'Please  enter a valid number';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (widget._formKey1.currentState!.validate()) {
+                      widget._formKey1.currentState!.save();
+                      if (_addrrss1Controller.text.isNotEmpty) {
+                        callService.makeOutboundCall(
+                            _addrrssController.text == ''
+                                ? '+91'
+                                : _addrrssController.text +
+                                    _addrrss1Controller.text);
+                      }
+                    }
+                  },
+                  child: const Text('Make a Call',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 38, 36, 71),
+                      )),
+                ),
+                Spacer(),
+                Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: const Text(
+                      "made by Pragetx Softwares",
+                      style: TextStyle(color: Colors.white54),
+                    ))
+              ])
+              // ListView.builder(
+              //   itemCount: users.length,
+              //   itemBuilder: (context, index) {
+              //     final user = users[index];
+              //     return ListTile(
+              //       leading: const Icon(Icons.person),
+              //       title: Text(user.name),
+              //       subtitle: Text(user.phone),
+              //       onTap: () => callService.makeOutboundCall(user.phone),
+              //     );
+              //   },
+              // ),
+              ),
         ),
       ),
     );
